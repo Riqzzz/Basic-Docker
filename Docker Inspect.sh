38e399ef8432a5b6ad33e4dff91a9a99d7b34fa2
Tariq Aziz <68262159+Riqzzz@users.noreply.github.com>
2025-03-21 23:33:57 +0700

    Inspect Docker(Image,Container,Volume,Network)

---
+++ Docker Inspect.sh
@@ -0,0 +1,9 @@
+inspect detail image : docker image inspect namaimage
+inspect detail container: docker container inspect namacontainer
+inspect detail volume: docker volume inspect namavolume
+inspect detail network: docker network inspect namanetwork
+contoh:
+docker image inspect mongo-express:latest
+docker container inspect mongodbexpress
+docker volume inspect mongorestore
+docker network inspect mongonetwork
