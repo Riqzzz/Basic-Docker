ee0513665bc78eaf004d99ff73acf4c24c4f1957
Tariq Aziz <68262159+Riqzzz@users.noreply.github.com>
2025-03-21 23:46:00 +0700

    Docker Prune(erase image,container,volume,network)

---
+++ Docker Prune.sh
@@ -0,0 +1,5 @@
+hapus semua container yg sudah stop: docker container prune
+hapus semua image yg tidak digunakan container: docker image prune
+hapus semua network yg tidak digunakan container: docker network prune
+hapus semua volume yg tidak digunakan container: docker volume prune
+hapus image, container, network yg sudah tidak digunakan: docker system prune
